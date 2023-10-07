a79b5ec9b396412466aef57642da1b1c2404fcb5
DavixVulcan <Davix1648@gmail.com>
2023-10-07 14:53:14 -0500

    added dummy img, loaded .env file

--- HackUTA.py
+++ HackUTA.py
@@ -1,8 +1,10 @@
 
 #How to import your module from the modules folder
 from modules import test 
-#Used to inport env token
+
+#Used to inport env API token
 from dotenv import load_dotenv
+load_dotenv('.env')
 
 #Call module using modulename.modulefunction()
 test.initial_test()
