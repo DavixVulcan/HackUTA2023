bf752e0e4f086bcb3fccea515f6bc356693c7ce6
DavixVulcan <Davix1648@gmail.com>
2023-10-07 14:45:04 -0500

    Added python env module to requirements, and module to main script

--- HackUTA.py
+++ HackUTA.py
@@ -1,6 +1,8 @@
 
 #How to import your module from the modules folder
 from modules import test 
+#Used to inport env token
+from dotenv import load_dotenv
 
 #Call module using modulename.modulefunction()
 test.initial_test()
