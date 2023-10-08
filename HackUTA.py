29a5bea19dbe973a6c2d10c7eebd8b900ec8774a
Sami <samnik200@gmail.com>
2023-10-08 05:46:46 -0500

    changed image res

--- HackUTA.py
+++ HackUTA.py
@@ -6,22 +6,22 @@ import json
 
 #Call module using modulename.modulefunction()
 # test.initial_test()
+whiler = True
+while(whiler):
+    camera_main_task.take_picture("img_camera_out/MainPic.png")
+    camera_image_url = gcp_bucket_manager.push_image("img_camera_out/MainPic.png")
+    base_prompt = img2text_api.get_prompt(camera_image_url)
+    base_prompt = base_prompt.strip()
+    # print(base_prompt)
 
 
-camera_main_task.take_picture("img_camera_out/MainPic.png")
-camera_image_url = gcp_bucket_manager.push_image("img_camera_out/MainPic.png")
-base_prompt = img2text_api.get_prompt(camera_image_url)
-base_prompt = base_prompt.strip()
-# print(base_prompt)
-
-
-f = open("prompt_appends.json")
-array_urls = json.load(f)
-for i, y in array_urls.items():
-    # print(camera_image_url)
-    print(y)
-    output_image_url_1 = gcp_bucket_manager.process_image(camera_image_url, base_prompt + ", " + y)
-    #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
-    #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
-    #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
-    qr_generator.create_qr(output_image_url_1, i)
+    f = open("prompt_appends.json")
+    array_urls = json.load(f)
+    for i, y in array_urls.items():
+        # print(camera_image_url)
+        print(y)
+        output_image_url_1 = gcp_bucket_manager.process_image(camera_image_url, base_prompt + ", " + y)
+        #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
+        #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
+        #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
+        qr_generator.create_qr(output_image_url_1, i)
