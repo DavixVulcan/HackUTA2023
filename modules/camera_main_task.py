e932da5216f611f30d579e8dabe19c83a6d50815
Sami <samnik200@gmail.com>
2023-10-08 04:34:43 -0500

    changed image res

--- modules/camera_main_task.py
+++ modules/camera_main_task.py
@@ -1,4 +1,4 @@
 # Main function of the ArduCam Hawkeye
 import os
 def take_picture(location):
-    os.system("libcamera-jpeg -t 4000 --viewfinder-width 2312 --viewfinder-height 1736 -o "+location+" -q 25")
+    os.system("libcamera-jpeg -t 4000 --viewfinder-width 2312 --viewfinder-height 1736 --width 1920 --1080 --mode 1920:1080 -o "+location)
