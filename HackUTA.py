cd56ba1082bd952d9315696b6b8c1ce1756a7262
Sami <samnik200@gmail.com>
2023-10-08 05:35:53 -0500

    changed image res

--- HackUTA.py
+++ HackUTA.py
@@ -8,8 +8,8 @@ import json
 # test.initial_test()
 
 
-camera_main_task.take_picture("img_camera_out/MainPic.jpg")
-camera_image_url = gcp_bucket_manager.push_image("img_camera_out/MainPic.jpg")
+camera_main_task.take_picture("img_camera_out/MainPic.png")
+camera_image_url = gcp_bucket_manager.push_image("img_camera_out/MainPic.png")
 base_prompt = img2text_api.get_prompt(camera_image_url)
 base_prompt = base_prompt.strip()
 # print(base_prompt)
@@ -18,7 +18,7 @@ base_prompt = base_prompt.strip()
 f = open("prompt_appends.json")
 array_urls = json.load(f)
 for i, y in array_urls.items():
-    print(camera_image_url)
+    # print(camera_image_url)
     print(y)
     output_image_url_1 = gcp_bucket_manager.process_image(camera_image_url, base_prompt + ", " + y)
     #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
--- modules/camera_main_task.py
+++ modules/camera_main_task.py
@@ -1,4 +1,4 @@
 # Main function of the ArduCam Hawkeye
 import os
 def take_picture(location):
-    os.system("libcamera-still -t 4000 --viewfinder-width 1920 --viewfinder-height 1080 --width 1280 --height 720 --mode 1280:720 -o "+location)
+    os.system("libcamera-still -e png -t 4000 --viewfinder-width 1920 --viewfinder-height 1080 --width 1280 --height 720 --mode 1280:720 -o "+location)
