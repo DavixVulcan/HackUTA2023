7eb42af75768046ff2d06ed59f36bbea41e56d7c
DavixVulcan <Davix1648@gmail.com>
2023-10-08 00:13:35 -0500

    added img2text file

--- HackUTA.py
+++ HackUTA.py
@@ -7,10 +7,10 @@ from modules import test, img2img_api, qr_generator
 # test.initial_test()
 
 whiler = True
-while(whiler):
+# while(whiler):
     #take_picture("img_camera_out/MainPic.png")
     #imgurl1 = push_img("img_camera_out/MainPic.png")
-    img2img_api.get_api_image("img_camera_out/PNG_Test.png", "Spider-Man hanging from the side of the frame")
+img2img_api.get_api_image("img_camera_out/spidey.png", "Spider-Man hanging from the side of the frame")
     #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
     #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
     #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
