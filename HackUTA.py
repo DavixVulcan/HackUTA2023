bf9378adda153062bdcb6024d757c748dd2b430f
DavixVulcan <Davix1648@gmail.com>
2023-10-08 02:07:43 -0500

    added samis changes

--- HackUTA.py
+++ HackUTA.py
@@ -1,6 +1,6 @@
 
 #How to import your module from the modules folder
-from modules import test, img2img_api, qr_generator, gcp_bucket_manager
+from modules import test, img2img_api, qr_generator, gcp_bucket_manager, img2text_api
 
 
 #Call module using modulename.modulefunction()
@@ -10,7 +10,9 @@ whiler = True
 # while(whiler):
 # while(whiler):
     #take_picture("img_camera_out/MainPic.png")
+
 camera_image_url = gcp_bucket_manager.push_image("img_camera_out/raul.png")
+img2text_api.get_prompt(camera_image_url)
 output_image_url_1 = gcp_bucket_manager.process_image(camera_image_url, "Student in a classroom, realistic, high definition")
     #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
     #img2img_api.get_api_image("imgurl1", "Spider-Man hanging from the side of the frame")
