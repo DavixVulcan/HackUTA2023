d4f636a121889b6cbc8d71c481813cba3d16af0b
Sami <samnik200@gmail.com>
2023-10-08 03:30:42 -0500

    added key input for picture

--- modules/camera_main_task.py
+++ modules/camera_main_task.py
@@ -1,4 +1,4 @@
 # Main function of the ArduCam Hawkeye
 import os
 def take_picture(location):
-    os.system("libcamera-still -t 0 --viewfinder-width 2312 --viewfinder-height 1736 -o /"+location)
+    os.system("libcamera-still -t 0 --viewfinder-width 2312 --viewfinder-height 1736 -o /"+location+" -k")
